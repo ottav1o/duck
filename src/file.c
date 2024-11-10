4786cfc632bd376f5ea56a95a1bca3a34a17edd5
Ottávio <176840796+ottav1o@users.noreply.github.com>
2024-11-10 15:28:06 -0400

    bugfixes

--- src/file.c
+++ src/file.c
@@ -17,28 +17,33 @@ int read_file(const char *filename) {
     char *buffer = malloc(sizeof(char) * 8);
     if (buffer == NULL) {
         fclose(fptr);
-        assert(buffer != NULL);
+        fprintf(stderr, "ERROR: Cannot allocate necessary memory for text buffer.\n");
+        return 1;
     }
     
-    size_t buffer_size =  8;
+    size_t buffer_size = 8;
     
     int ch;
-    size_t i = 0;
-    do {
-        ch = fgetc(fptr);
-        
-        if (i == buffer_size - 1) {
+    size_t i;
+    for (i = 0; (ch = getc(fptr)) != EOF; i++) {
+        if (i >= buffer_size) {
             buffer_size += 8;
             buffer = realloc(buffer, sizeof(char) * buffer_size);
+            if (buffer == NULL) {
+                fprintf(stderr, "ERROR: Cannot reallocate necessary memory for text buffer.\n");
+                return 1;
+            }
         }
+
+        if (ch == EOF)
+            break;
         
         buffer[i] = ch;
-        
-        i++;
-    } while (ch != EOF);
+    }
     buffer[i] = '\0';
 
     printf("%s\n", buffer);
+    puts("-- End of File --");
     
     return 0;
 }
